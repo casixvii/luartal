70bed28e33ca8f03b8f1759e407dd19d2b840117
Casix <mvpcasix@gmail.com>
2025-12-25 15:48:56 +0500

    Update inp.c

--- inp.c
+++ inp.c
@@ -4,7 +4,7 @@
 
 int main() {
     int timeout_ms = 100; // this WILL murder someone's hard drive. But smooth game
-v    int key = 0;
+    int key = 0;
 
     for (int i = 0; i < timeout_ms / 5; i++) {
         if (_kbhit()) {
