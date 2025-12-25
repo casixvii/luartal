ab3ab3c41434e1f9099f1649560076bc187748fb
Casix <mvpcasix@gmail.com>
2025-12-25 11:07:28 +0500

    Update inpipe.bat

--- inpipe.bat
+++ inpipe.bat
@@ -1,2 +1,3 @@
-inp.exe > uinp.jof
-REM i know that this is a very, very weird way to get user input. but it works!!
+@echo off
+inp.exe > uinp.bin
+REM i know that this is a very, very weird way to get user input. but this is all i have for smooth game
