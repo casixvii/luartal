fbf0662bd6672288d51002413ffdeb6578e8efc9
Casix <mvpcasix@gmail.com>
2025-12-25 14:16:32 +0500

    Update inp.c

--- inp.c
+++ inp.c
@@ -1,9 +1,24 @@
 #include <conio.h>
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
-	int key = getch();
-	fputc(key, stdout);
-	fflush(stdout);
-	return 0;
+    int timeout_ms = 100; // this WILL murder someone's hard drive. But smooth game
+v    int key = 0;
+
+    for (int i = 0; i < timeout_ms / 5; i++) {
+        if (_kbhit()) {
+            key = getch();
+            break;
+        }
+        Sleep(5);
+    }
+
+    if (key == 0) {
+        key = 'g';
+    }
+
+    putchar(key);
+    fflush(stdout);
+    return 0;
 }
